ea565e6cb7b5fcf85d11ba7bf329a7e10178f8f0
Christian Tobias <cwtobias6@gmail.com>
2015-07-30 10:53:30 -0500

    functions mysql

---
+++ functions_exercises.sql
@@ -0,0 +1,28 @@
+
+SELECT count(*)
+FROM employees
+WHERE first_name IN('Irena','Vidya','Maya')
+GROUP BY gender;
+
+SELECT concat(last_name, ',',first_name) AS E_names
+FROM employees
+WHERE (last_name LIKE 'E%') AND (last_name LIKE '%E')
+ORDER BY emp_no ASC;
+
+SELECT last_name,datediff(now(),hire_date) AS days_working_here
+FROM employees
+WHERE (birth_date LIKE '%-12-25') AND (hire_date BETWEEN '1990-01-01' and '1999-12-31')
+ORDER BY birth_date ASC, hire_date ASC;
+
+
+SELECT concat(last_name,' ', first_name) AS names,count(last_name) AS number_of_ppl_with_last_name
+FROM employees
+WHERE (last_name LIKE '%q%') AND (last_name NOT LIKE '%qu%')
+GROUP BY last_name;
+
+
+
+
+
+
+
